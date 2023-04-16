8c3327f3a292715c7a441deb825a94f55809f9d1
Bernard R <contact.jumpix@gmail.com>
2023-04-16 18:59:45 +0200

    Commit du jour 16/04/23

---
+++ wp-content/themes/planty-child/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Planty
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-2',
+					'menu_id'        => 'footer-menu',
+				)
+			);
+			?>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- wp-content/themes/planty-child/functions.php
+++ wp-content/themes/planty-child/functions.php
@@ -4,9 +4,9 @@ function planty_child_setup() {
   
 	register_nav_menus(
 		array(
+			'menu-1' => esc_html__( 'Header', 'planty' ),
 			'menu-2' => esc_html__( 'Footer', 'planty' ),
 		)
 	);
-
 }
 add_action( 'after_setup_theme', 'planty_child_setup' );
