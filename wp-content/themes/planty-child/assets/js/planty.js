8d6748e983d61bdd984d8a29d9effd8ea70331eb
Bernard R <contact.jumpix@gmail.com>
2023-04-24 11:58:45 +0200

    Commit du jour 24/04/23

---
+++ wp-content/themes/planty-child/assets/js/planty.js
@@ -0,0 +1,24 @@
+// Sélectionnez tous les boutons de quantité
+var quantityButtons = document.querySelectorAll(".quantity-button");
+
+// Bouclez sur tous les boutons de quantité et ajoutez un écouteur d'événement click à chacun
+quantityButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    // Obtenez le champ de nombre correspondant
+    var numberField = this.parentNode.parentNode.querySelector(".quantity .number-style");
+
+    // Obtenez la valeur actuelle du champ de nombre
+    var currentValue = parseInt(numberField.value);
+
+    // Vérifiez si le bouton cliqué est le bouton "plus" ou "moins"
+    if (this.classList.contains("quantity-up")) {
+      // Augmentez la valeur du champ de nombre de 1
+      numberField.value = currentValue + 1;
+    } else {
+      // Vérifiez si la valeur actuelle est supérieure à 0 avant de la diminuer de 1
+      if (currentValue > 0) {
+        numberField.value = currentValue - 1;
+      }
+    }
+  });
+});
--- wp-content/themes/planty-child/functions.php
+++ wp-content/themes/planty-child/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.1' );
+}
+
 /**
  * Add Menu Header Footer
  */
@@ -34,7 +39,7 @@ function planty_child_scripts()
 {
   wp_enqueue_style('font-syne', 'https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap');
   wp_enqueue_style('planty-style', get_stylesheet_uri(), array(), _S_VERSION);
-  wp_enqueue_script('planty-js', get_template_directory_uri() . '/js/planty.js', array(), _S_VERSION, true);
+  wp_enqueue_script('planty-js', get_stylesheet_directory_uri() . '/assets/js/planty.js', array(), _S_VERSION, true);
 }
 add_action('wp_enqueue_scripts', 'planty_child_scripts');
 
@@ -42,7 +47,7 @@ function planty_child_before_admin()
 {
   wp_enqueue_style('font-syne', 'https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap');
   wp_enqueue_style('planty-style', get_stylesheet_uri(), array(), _S_VERSION);
-  wp_enqueue_script('planty-js', get_template_directory_uri() . '/js/planty.js', array(), _S_VERSION, true);
+  wp_enqueue_script('planty-js', get_stylesheet_directory_uri() . '/assets/js/planty.js', array(), _S_VERSION, true);
 }
 add_action('enqueue_block_editor_assets', 'planty_child_scripts');
 
